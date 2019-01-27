253d5b1d3a6d89d3d92e817d1a21895e52d0d5bc
Antonio Jr <acdcjunior@gmail.com>
2019-01-27 20:44:55 -0200

    feat(gitlab): impl getGitLabCommit()

--- src/gitlab/getGitLabCommit.ts
+++ src/gitlab/getGitLabCommit.ts
@@ -1,19 +1,10 @@
 import axios from 'axios';
 import config from '../config';
-import {GitLabCommit} from "../../../ports/outbound/types/GitLabCommit/GitLabCommit";
-import {OutboundPorts} from "../../../ports/outbound/OutboundPorts";
-import {encodeGroupProjectPath} from "./encodeGroupProjectPath";
+import {GitLabCommit} from "./types/GitLabCommit/GitLabCommit";
 
 
-async function getGitLabCommit(project_id: string | number, sha: string): Promise<GitLabCommit> {
-    let {data: commit} = await axios.get(
-        `${config.codeRevue.repoGitlab.gitlab.url}/api/v4/projects/${encodeGroupProjectPath(project_id)}/repository/commits/${sha}`,
-        {headers: {"PRIVATE-TOKEN": config.codeRevue.repoGitlab.gitlab.apiToken}}
-    );
+export async function getGitLabCommit(serverUrl: string, projectId: number | string, sha: string): Promise<GitLabCommit> {
+    let url = `${serverUrl}/api/v4/projects/${projectId}/repository/commits/${sha}`;
+    let {data: commit} = await axios.get(url, {headers: {"PRIVATE-TOKEN": config.codeRevue.gitlabApiToken}});
     return commit;
 }
-
-// noinspection JSUnusedLocalSymbols
-const variableToGuaranteeTheFunctionMatchesTheInterface: OutboundPorts['getGitLabCommit'] = getGitLabCommit;
-
-export { getGitLabCommit };
